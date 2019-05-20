8068c08fad72a057708fa32ead4b2f280a4b6d6f
AymanDev <maxsa21@gmail.com>
2019-05-20 13:25:17 +0300

    Workng timers
    Implemented and tested timers

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # PausableTimers
+
+
+
+### How to use this timers?
+Almost same as default `setTimeout` and `setInterval`.
+First need to declare timer variable:
+```js
+const timer = setPausableTimeout(() => console.log("message"), 5000);
+```
+This timeout will execute after 5 seconds.
+#### How to use interval timer?
+Just have to change function name little bit:
+```js
+const timer = setPausableInterval(() => console.log("message"), 5000);
+```
+
+### How to pause timer?
+Just have to call method from timer:
+```js
+timer.pauseTimer();
+```
+
+### How to unpause timer?
+Just have to call method from timer:
+```js
+timer.startTimer();
+```
---
+++ index.ts
@@ -0,0 +1,64 @@
+export enum TimerType {
+  INTERVAL = "interval",
+  TIMEOUT = "timeout"
+}
+
+export class PausableTimer {
+  private callback: Function;
+  private startTime: number;
+  public leftTime: number;
+  private endTime: number;
+  private betweenTime: number;
+  private type: TimerType;
+  private timer: number | null = null;
+
+  constructor(
+    callback: Function,
+    time: number,
+    type: TimerType = TimerType.TIMEOUT
+  ) {
+    this.callback = callback;
+    const rightNow = Date.now();
+    this.startTime = rightNow;
+    this.leftTime = time;
+    this.betweenTime = time;
+    this.endTime = rightNow + time;
+    this.type = type;
+  }
+
+  startTimer() {
+    if (this.type === TimerType.TIMEOUT) {
+      this.timer = setTimeout(this.callback, this.leftTime);
+    }
+    if (this.type === TimerType.INTERVAL) {
+      this.timer = setInterval(() => {
+        this.startTime = Date.now();
+        this.leftTime = this.betweenTime;
+        this.endTime = this.startTime + this.betweenTime;
+        this.callback();
+      }, this.leftTime);
+    }
+  }
+
+  pauseTimer() {
+    if (this.timer !== null) {
+      const rightNow = Date.now();
+      this.leftTime = this.endTime - rightNow;
+      clearTimeout(this.timer);
+    }
+  }
+}
+
+export function setPausableTimeout(callback: Function, time: number) {
+  const timer = new PausableTimer(callback, time, TimerType.TIMEOUT);
+  timer.startTimer();
+  return timer;
+}
+
+export function setPausableInterval(callback: Function, time: number) {
+  const timer = new PausableTimer(callback, time, TimerType.INTERVAL);
+  timer.startTimer();
+  return timer;
+}
+
+
